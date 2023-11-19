e9917c6db26f121e12e022cdbd02e8ee94dfdd48
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-11-19 22:27:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict";      // tret all JS code as newer version
+
+//alert("hello")   // we are using node.js not browser so this alert will not work
+
+
+
+// DATATYPES
+
+let name = "hitesh"        //String datatype
+let age = 18               //number datatype
+let isLoggedIn = true      //boolean datatype
+
+
+/*
+
+Primitive DataTypes
+
+1. number (upto 2^53)
+2. bigint (big integer values)
+3. string ("HELLO ji kese hoe?")
+4. boolean (true or false)
+
+5. null : it is a standalone value, represents an empty value
+6. undefined : value not assigned
+7. symbol : used for uniqueness
+*/
+
+
+
+console.log(typeof null);       //type of null is object
+
+console.log(typeof undefined);  // type of undefined is undefined
