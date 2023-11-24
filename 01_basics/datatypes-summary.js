01f160c94535b66b41f052020f9bbf9e724ec2f3
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-11-24 09:22:01 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+//JavaScript is dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+//humein btana nhi pdta ki variables declare krte huye ki ye number hai ya boolean hai, but in typescript we need to do this.
+
+
+
+//USING SYMBOLS in JS
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId)    // Gives false, even tough same number stored hai in dono mein lekin dono associated with unique id
+
+
+
+//USING BIGINT IN JS 
+
+
+
+
+
+
+/*
+DATATYPES ARE OF TWO TYPES :
+
+1. PRIMITIVE : call by value 
+             : they are of 7 types : a) String b)Number c) Boolean d) NULL e) Undefined f) Symbol g) BigInt
+
+  
+             
+2. NON-PRIMITIVE : call by Reference
+                 : a) array b) objects c)functions             
+*/
