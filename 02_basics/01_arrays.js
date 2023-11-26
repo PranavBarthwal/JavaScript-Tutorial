2951d1aa62d58a51684c9eba12cbd9660379aa44
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-11-26 12:55:00 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+// ARRAYS
+
+// Read from here : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+
+// DECLARING AND ACCESSING ARRAY 
+
+// const arr = [10, 20, 30, 40, 50, 60]
+// const arr2 = ["shaktiman", "batman", "nagraj"]
+
+//we can also use Array constructor function to create and array
+
+// const arr3 = new Array(1, 2, 3, 45, 56)
+
+// console.log(arr[4])
+// console.log(arr2[1])
+// console.log(arr3[4])
+
+
+
+
+// ARRAY METHODS
+
+const array = [10,20,30,40,50,60]
+
+//Push
+array.push(70)
+console.log(array)
+
+//Pop
+array.pop()
+console.log(array)
