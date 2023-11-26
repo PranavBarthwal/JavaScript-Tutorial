24f87a64042d73586407444e42d3f2622c80e0de
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-11-26 10:01:05 +0000

    date and time

--- 01_basics/09_dates&time.js
+++ 01_basics/09_dates&time.js
@@ -31,3 +31,45 @@
 
 
 // CREATING A SPECIFIC DATE
+
+// let myBday = new Date(2023, 0, 23, 5, 8)                  
+// console.log(myBday.toDateString())                  // gives Mon Jan 23 2023 (month starts from 0)
+// console.log(myBday.toLocaleString())                // gives 1/23/2023, 5:08:00 AM
+
+//in specific formats like yyyy-mm-dd
+// let myAnni = new Date("2023-11-15")
+// console.log(myAnni.toDateString())                  // gives Wed Nov 15 2023
+ 
+
+
+
+// CREATING TIMESTAMPS IN JS
+
+let myGymDate = new Date("2023-11-09")
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp)                               // gives 1700991708482 (it is miliseconds from 1 january 1970 till now)
+
+console.log(myGymDate.getTime())                       // gives 1699488000000 (is date tk ke milisecconds)
+
+// Convert these miliseconds into seconds
+console.log(Math.floor(Date.now()/1000))               // gives 1700992210
+
+
+
+
+// METHODS IN DATE OBJECT
+ 
+let newDate = new Date()
+
+console.log(newDate.getDay())
+console.log(newDate.getMonth())
+console.log(newDate.getMinutes())
+
+
+
+// Make customised formats using toLocaleString() method
+
+newDate.toLocaleString('dafault' {
+    weekday : "long",
+})
