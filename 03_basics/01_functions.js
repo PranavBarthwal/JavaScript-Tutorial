55de2e91a84afa0c770bdb5830022de4a83cbac0
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-11-27 10:32:29 +0000

    functions basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+// Declaring a function
+
+// function sayMyName(){
+//     console.log("H");
+//     console.log("I");
+//     console.log("T");
+//     console.log("E");
+//     console.log("S");
+//     console.log("H");
+// }
+
+// sayMyName                     // fucntion ka Reference
+
+// sayMyName()                   // function ka execution, gives the whole name in new lines
+
+
+
+
+//
+
+function addTwoNumbers(number1, number2){                      // here number1 & number2 are parameters
+
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(3,5)                                              // here 3 & 5 are arguments, we get 8
