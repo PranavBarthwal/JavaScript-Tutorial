be9eade8adc2d6c774362f6582b5b94116c34780
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-11-27 08:04:13 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,57 @@
+// JAVASCRIPT OBJECTS
+
+// they can be declared through literals or through constructors
+
+
+// CONSTRUCTOR METHOD
+// SINGLETON : one of its kind, formed when created through constructor
+//object.create
+
+
+
+// LITERALS METHOD
+
+
+const mySym = Symbol("key1")
+
+const user = {
+    name : "hitesh",
+    age : 19,
+    "employee Id" : "GOOGLEDEL5746",                   // this type of element cannot be accessed through user.employee Id method
+    location : "jaipur",
+    isLoggedIn : false,
+    email : "hitesh@gmail.com",
+    lastLoginDays : ["monday" , "saturday"],
+    [mySym] : "mykey10",
+}
+
+// ACCESSING ELEMENTS OF OBJECT
+
+// 1st Method
+// console.log(user.email)
+// console.log(user.age)
+// console.log(user.employee Id)                  // gives error
+
+// 2nd Method
+// console.log(user["email"])
+// console.log(user["age"])
+// console.log(user["employee Id"])
+console.log(user[mySym])
+
+// typeof this mySym
+console.log(typeof user[mySym])                 //gives string, normally agar hum symbol ko use kre object mein to vo as a symbol nhi blki as a string store hota hai. therefore we have a specific syntax for using symbols in objects. [mySym] = "myKey10"
+
+
+
+// changing object values
+user.name = "durgesh"
+
+Object.freeze(user)                    // is object ko freeze krdia, ab ismein changes nhi honge
+
+user.name = "shwetabh"                 // ye name change nhi hoga
+
+console.log(user)
+
+
+
+// Creating a funciton
