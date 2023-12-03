ca9c3528b0a0ff40e3534024c7bf482d30cae80e
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-12-03 19:46:52 +0000

    arrow function done

--- 03_basics/04_arrow&this.js
+++ 03_basics/04_arrow&this.js
@@ -33,3 +33,57 @@ gives hitesh , welcome to the website
 
 
 //***********************************************************************************************************************************************************************
+
+// normal fucntion : mein this nhi use ho pa rha
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this)                              // gives bhot saari values   
+//     console.log(this.username)                     // gives undefined, as functions mein this use nhi ho paa rha
+// }
+
+// chai()
+
+
+// Const mein held fucntion mein bhi this use nhi ho paa rha
+// function coffee(){
+//     let username = "hitesh"
+//     console.log(this)                              // gives bhot saari values   
+//     console.log(this.username)                     // gives undefined, as functions mein this use nhi ho paa rha
+// }
+
+// coffee()
+
+
+// arrow fucntion mein bhi this use nhi ho pa rha
+// const biscuit = () => {
+//     let usernmae = "hitesh"
+//     console.log(this)                               // gives {}
+//     console.log(this.username)                      // gives undefined
+// }
+
+// biscuit()
+
+
+
+
+//*******************************************************ARROW FUNCTION***************************************************************************************
+
+// Basic Syntax
+// const addTwo = (num1, num2) => {
+//     return num1+num2
+// }
+
+// console.log(addTwo(3,4))
+
+
+
+// OTHER WAYS OF CREATING ARROW FUCNTION
+
+// const add1 = (num1) => (num1+1)               // ismein return likhne ki zrurat nhi h
+// console.log(add1(3))
+
+
+// returning object in arrow function
+const user1 = (num1) => ({username:"hitesh"})               
+console.log(user1(3))                   // gives {username:"hitesh"}
+ 
