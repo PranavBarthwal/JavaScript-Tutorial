e03522d67654f542057a8da61a83b8f38f9e7623
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-12-05 19:14:41 +0000

    for each loop

---
+++ 05_iterations.js/arrayLoops3.js
@@ -0,0 +1,70 @@
+// for each loop
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+//syntax of for each loop : it takes a callback function as input (callback function ka koi name nhi hota and parenthesis ke andar ek parameter hota hai)
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+
+// we can give this callback function in the form of arrow function
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+
+
+// we can also give an external function
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)         // yha printme function ka reference pass kra h. pura function call nhi kra (printme())
+
+
+
+
+
+
+
+// the function in for each loop has access to many parameters other than item, like index or the whole array 
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+
+
+
+
+
+
+
+// accessing elements in array of objects using for each loop
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
+
+
+
