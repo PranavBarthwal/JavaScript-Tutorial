6f8d82cf30e999186c936d1f2ae05e4491957034
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-12-06 11:03:44 +0000

    reduce

--- 05_iterations.js/08_reduce.js
+++ 05_iterations.js/08_reduce.js
@@ -13,3 +13,54 @@ The callback for Reduce has two parameters: the accumulator and the current valu
 
 
 // ****************************************************************************************
+
+
+// REDUCE
+//The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.  The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place. Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
+
+
+//printing sum of array using reduce
+
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce( function(accumulator, current) {
+//     console.log(`accumulator : ${accumulator} and Current : ${current}`);
+//     return accumulator +  current
+// },0)
+
+
+// writing same thing using arow function
+const myTotal = myNums.reduce((acc, curr) => (acc+curr),0 )
+
+console.log(myTotal)
+
+
+
+
+
+
+// real life us ecase of reduce in shopping cart
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+
+const totalPrice = shoppingCart.reduce((acc, item) => (acc + item.price),0)
+console.log(totalPrice)
