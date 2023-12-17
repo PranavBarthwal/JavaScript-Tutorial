086bf6d49ad7d8b55f375df7f4bb7672b5a0ebc0
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-12-17 17:51:57 +0530

    keyboard magic mini proj

---
+++ 09_async/04_async_keyboard_magic/chaiaurcode.js
@@ -0,0 +1,17 @@
+const text = document.querySelector('#insert')
+
+document.addEventListener('keydown', function(e){
+    text.innerHTML = 
+    `<table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  </table>`
+})
