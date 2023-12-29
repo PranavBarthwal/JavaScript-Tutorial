fb2a85f21ca4fe1d04ebe97d0de34597e1f1019d
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2023-12-29 15:22:10 +0000

    prototype done

--- 11_classesAndOOPS/03_prototype.js
+++ 11_classesAndOOPS/03_prototype.js
@@ -20,7 +20,7 @@ function mulBy5(num){
 mulBy5.power = 2            // ab ye dot notation to hum object ke liye use krte hai 
 
 console.log(mulBy5(5))
-console.log(mulBy5.power)              // iska output 2 agya i.e. fucntions are also objects
+console.log(mulBy5.power)              // iska output 2 agya i.e. fucntions are also objects. functions are both function and object
 console.log(mulBy5.prototype)          // gives {} , ye actually us fucntion ka this (current context) hai
 
 
@@ -84,3 +84,122 @@ The new object is returned: After the constructor function has been called, if i
 
 
 //**********************Prototype************************************* 
+
+// now if we have a string and we want to know its length so we'll use .length property. but if our string has empty spaces so we'll get wrong values so we want to create a method that will give true lenght of th string
+// eg : let myName = "hitesh     "
+    //  let myChannel = "chaiaurcode     "
+    //  console.log(myName.trueLength)
+
+
+
+
+
+// creating an array
+let myHeros = ["thor", "spiderman"]
+
+// creating an object
+let heroPower = {
+    thor : "hammer",
+    spiderman : "sling",
+
+    getSpiderPower : function(){
+        console.log(`spidy power is ${this.spiderman}`)
+    }
+}
+
+
+// creating our own method in objects's prototype, instead of adding it in myHeros's or heroPower's prototype, bcoz after all they both are array and object (having parent as object), so they can access its properties.
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present here`)
+}
+
+
+// checking if this property works for our object
+heroPower.hitesh()           // gives hitesh is present here
+
+// checking if this property works for our array 
+myHeros.hitesh()             // gives hitesh is present here
+
+
+
+// now creating a method in array's prototype and seeing if array and object is able to acces that property
+
+Array.prototype.heyHitesh = function(){
+    console.log("hitesh says hello JI")
+}
+
+// checking if this property works for our object
+//heroPower.heyHitesh()           // throws error, so object cant access arrays property as object is arrays parent
+
+// checking if this property works for our array 
+myHeros.heyHitesh()             // gives hitesh says hello JI
+
+
+
+
+
+
+
+//**************************INHERITANCE***************************
+//In JavaScript, prototypal inheritance is a fundamental mechanism that enables objects to inherit properties and methods from other objects. This inheritance is established through a chain of prototypes, often referred to as the "prototype chain." Each object has a prototype, and if a property or method is not found in an object, JavaScript looks for it in its prototype, and the search continues up the chain.
+
+
+const user = {
+    name : "hitesh",
+    email : "chai@google.com"
+}
+
+const teacher = {
+    makevideo : true
+}
+ 
+const teachingSupport = {
+    isAvailable : true
+}
+
+const TASupport = {
+    makeAssignment : "JS Assignment",
+    fulltime : true,
+
+    // giving this object all the properties of TeachingSupport Object
+    __proto__ : teachingSupport
+}
+
+// we can also do this outside the object.
+teacher.__proto__ = user             // teacher object ko user object ki properties ka access dediya
+
+
+// modern syntax of __proto__
+Object.setPrototypeOf(teachingSupport, teacher)      // teachingSupport ko teacher ki properties ka access dediaa
+
+
+
+
+
+
+
+
+// now finally solving this problem
+// now if we have a string and we want to know its length so we'll use .length property. but if our string has empty spaces so we'll get wrong values so we want to create a method that will give true lenght of th string
+// eg : let myName = "hitesh     "
+    //  let myChannel = "chaiaurcode     "
+    //  console.log(myName.trueLength)
+
+
+
+let newName = "chaiAurCide     "
+String.prototype.trueLength = function(){                          // saari string ke prototype mein daaldia ye method
+    console.log(this)
+    console.log(`True Length of this string is ${this.trim().length}`)
+
+}
+
+
+newName.trueLength()          // gives True Length of this string is 11
+
+
+// we can now use it in any string
+"jhbjh mndb jhdbmnd kj,m    kcbekc ,mnbnkjce ".trueLength()         // gives True Length of this string is 43
+
+
